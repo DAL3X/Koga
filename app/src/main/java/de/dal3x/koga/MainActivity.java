98fe5d07626902738dd15128c9915b8b80f9bc3c
Simon Bothe <uzifr@student.kit.edu>
2025-02-07 23:47:17 +0100

    implemented main activity button functionality

--- app/src/main/java/de/dal3x/koga/MainActivity.java
+++ app/src/main/java/de/dal3x/koga/MainActivity.java
@@ -1,6 +1,8 @@
 package de.dal3x.koga;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import de.dal3x.koga.options.Options;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Options options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        loadOptions();
+
+        Button kogaButton = findViewById(R.id.button_koga);
+        kogaButton.setOnClickListener(view -> startActivity(KogaActivity.class));
+        Button addMenuButton = findViewById(R.id.button_addmenu);
+        addMenuButton.setOnClickListener(view -> startActivity(AddActivity.class));
+        Button listMenuButton = findViewById(R.id.button_listmenu);
+        listMenuButton.setOnClickListener(view -> startActivity(ListActivity.class));
+        Button optionButton = findViewById(R.id.button_options);
+        optionButton.setOnClickListener(view -> startActivity(OptionsActivity.class));
+    }
+
+    // Starts an activity with all options bundled up
+    private void startActivity (Class<?> activityClass) {
+        Intent intent = new Intent(this, activityClass);
+        intent.putExtra("numberDays", this.options.getNumberDays());
+        intent.putExtra("numberMeat", this.options.getNumberMeat());
+        intent.putExtra("maxDuplicate", this.options.getMaxDuplicate());
+        intent.putExtra("maxCalorieScore", this.options.getMaxCalorieScore());
+        intent.putExtra("maxCarbDuplicates", this.options.getMaxCarbDuplicates());
+        startActivity(intent);
+    }
+
+    private void loadOptions() {
+        // TODO load options from device
+       this.options = new Options();
     }
 }
