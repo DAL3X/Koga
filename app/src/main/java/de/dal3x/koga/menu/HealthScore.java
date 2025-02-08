c3aaaba2e5431fdf66db5c0a4e9f1e5afe0fbdcb
DAL3X <simon.bothe@gmx.de>
2025-02-08 15:52:36 +0100

    add explicit values to health scores

--- app/src/main/java/de/dal3x/koga/menu/HealthScore.java
+++ app/src/main/java/de/dal3x/koga/menu/HealthScore.java
@@ -1,7 +1,13 @@
 package de.dal3x.koga.menu;
 
 public enum HealthScore {
-    HEALTHY,
-    NORMAL,
-    UNHEALTHY;
+    HEALTHY(0),
+    NORMAL(1),
+    UNHEALTHY(2);
+
+    public final int number;
+
+    private HealthScore(int number) {
+        this.number = number;
+    }
 }
