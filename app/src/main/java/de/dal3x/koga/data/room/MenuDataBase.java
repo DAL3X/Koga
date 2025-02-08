8d2d00a73539aae5ad9dd42f5f83445f2fab4a80
DAL3X <simon.bothe@gmx.de>
2025-02-08 22:53:45 +0100

    fixed menu data base type converter annotation

--- app/src/main/java/de/dal3x/koga/data/room/MenuDataBase.java
+++ app/src/main/java/de/dal3x/koga/data/room/MenuDataBase.java
@@ -2,10 +2,12 @@ package de.dal3x.koga.data.room;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 import de.dal3x.koga.menu.Menu;
 
 @Database(entities = {Menu.class}, version = 1, exportSchema = false)
+@TypeConverters({RecipeConverter.class})
 public abstract class MenuDataBase extends RoomDatabase {
     public abstract MenuDAO menuDAO();
 }
