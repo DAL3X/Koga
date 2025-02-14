5fa94bc367530907b121713e370aaf17a7243083
DAL3X <simon.bothe@gmx.de>
2025-02-14 18:09:12 +0100

    update carbohydrates to comply with new strings

--- app/src/main/java/de/dal3x/koga/menu/constants/Carbohydrate.java
+++ app/src/main/java/de/dal3x/koga/menu/constants/Carbohydrate.java
@@ -1,10 +1,30 @@
 package de.dal3x.koga.menu.constants;
 
+import android.content.Context;
+
+import de.dal3x.koga.R;
+
 public enum Carbohydrate {
     PASTA,
     RICE,
     POTATO,
     BREAD,
     OTHER,
-    NO
+    NONE;
+
+    public String getString(Context context) {
+        switch(this) {
+            case PASTA:
+                return context.getResources().getString(R.string.carbohydrate_pasta);
+            case RICE:
+                return context.getResources().getString(R.string.carbohydrate_rice);
+            case POTATO:
+                return context.getResources().getString(R.string.carbohydrate_potato);
+            case BREAD:
+                return context.getResources().getString(R.string.carbohydrate_bread);
+            case OTHER:
+                return context.getResources().getString(R.string.carbohydrate_other);
+        }
+        return context.getResources().getString(R.string.carbohydrate_nothing);
+    }
 }
